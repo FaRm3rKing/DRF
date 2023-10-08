864da3d6753a3e0de3f6837f693d70019a95e1cb
FaRm3rKing <re.georgejrnm@protonmail.com>
2023-10-08 23:05:13 +0800

    api views

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
-    path('api/', incllude('blog_api.urls', namespace='blog_api'),)
+    path('api/', include('blog_api.urls', namespace='blog_api'),)
 ]
